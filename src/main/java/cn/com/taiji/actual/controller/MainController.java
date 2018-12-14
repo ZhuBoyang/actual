6d4576396b56ca9946d72333fff41cce10061a2d
zxx <609747994@qq.com>
2018-12-14 13:15:41 +0800

    添加主页样式

--- src/main/java/cn/com/taiji/actual/controller/MainController.java
+++ src/main/java/cn/com/taiji/actual/controller/MainController.java
@@ -1,9 +1,18 @@
 package cn.com.taiji.actual.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 /**
  * @Author: zxx
  * @Date: 2018/12/14 11:38
  * @Version 1.0
  */
+@Controller
 public class MainController {
+
+    @GetMapping("/index")
+    public String index(){
+        return "index";
+    }
 }
