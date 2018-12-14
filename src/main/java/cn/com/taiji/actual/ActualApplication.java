3b7381b6776b225c5b977817d8be025318e1067a
Barry <903103398@qq.com>
2018-12-14 10:45:37 +0800

    Initial commit

---
+++ src/main/java/cn/com/taiji/actual/ActualApplication.java
@@ -0,0 +1,14 @@
+package cn.com.taiji.actual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActualApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActualApplication.class, args);
+    }
+
+}
+
