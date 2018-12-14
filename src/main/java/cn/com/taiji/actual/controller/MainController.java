eb54204ca0cec7800985cb530e3481e9f4193af2
zxx <609747994@qq.com>
2018-12-14 14:46:49 +0800

    修改作者注释

--- src/main/java/cn/com/taiji/actual/controller/MainController.java
+++ src/main/java/cn/com/taiji/actual/controller/MainController.java
@@ -1,9 +1,9 @@
 package cn.com.taiji.actual.controller;
 
 /**
- * @Author: zxx
- * @Date: 2018/12/14 11:38
- * @Version 1.0
+ * @author zxx
+ * @date 2018/12/14 11:38
+ * @version 1.0
  */
 public class MainController {
 }
--- src/main/java/cn/com/taiji/actual/domain/User.java
+++ src/main/java/cn/com/taiji/actual/domain/User.java
@@ -1,9 +1,9 @@
 package cn.com.taiji.actual.domain;
 
 /**
- * @Author: zxx
- * @Date: 2018/12/14 10:52
- * @Version 1.0
+ * @author zxx
+ * @date 2018/12/14 10:52
+ * @version 1.0
  */
 public class User {
 
