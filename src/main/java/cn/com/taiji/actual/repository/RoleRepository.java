c01c92a34c7819f94fa452affbe7961ace800292
zxx <609747994@qq.com>
2018-12-17 16:18:01 +0800

    角色相关功能

--- src/main/java/cn/com/taiji/actual/repository/RoleRepository.java
+++ src/main/java/cn/com/taiji/actual/repository/RoleRepository.java
@@ -3,11 +3,23 @@ package cn.com.taiji.actual.repository;
 import cn.com.taiji.actual.domain.Role;
 import cn.com.taiji.actual.domain.UserInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 /**
  * @author zxx
  * @date 2018/12/14 11:39
  * @version 1.0
  */
-public interface RoleRepository extends JpaRepository<Role,Integer> {
+public interface RoleRepository extends JpaRepository<Role,Integer>, JpaSpecificationExecutor<Role> {
+    /**
+     * 根据id删除(操作状态state=0)
+     * @param rid
+     * @param state
+     */
+    @Modifying
+    @Query("update Role set state=:state where rid=:rid")
+    void deleteById(@Param("rid") Integer rid, @Param("state") String state);
 }
